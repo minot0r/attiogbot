0095516e9fa5933c4e85d78b0bda60c7165d20e2
Giorgetti <giorgettivalentin@gmail.com>
2019-10-07 13:36:24 +0200

    Modified ui.js command

---
+++ commands/ui.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: 'ui',
+    description: 'ui ui, c\'est ça',
+    needArgs: 1,
+    execute(message, args) {
+        if(!isNaN(args[0]) && args[0] < 500)
+            message.channel.send('ui '.repeat(args[0]))
+    }
+}
--- commands/wui.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'wui',
-    description: 'wui wui, c\'est ça',
-    needArgs: 1,
-    execute(message, args) {
-        if(!isNaN(args[0]) && args[0] < 500)
-            message.channel.send('Wui' + 'i'.repeat(args[0]))
-    }
-}
